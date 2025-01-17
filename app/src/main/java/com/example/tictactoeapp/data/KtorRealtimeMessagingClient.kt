85093a2108b8d447188cc311e201eaef8d0d05e7
vicki <surajbarik2003@gmail.com>
2025-01-17 20:42:47 +0530

    implemented the functions to handle client

---
+++ app/src/main/java/com/example/tictactoeapp/data/KtorRealtimeMessagingClient.kt
@@ -0,0 +1,53 @@
+package com.example.tictactoeapp.data
+
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.websocket.webSocketSession
+import io.ktor.client.request.url
+import io.ktor.websocket.Frame
+import io.ktor.websocket.WebSocketSession
+import io.ktor.websocket.close
+import io.ktor.websocket.readText
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.filterIsInstance
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.mapNotNull
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+class KtorRealtimeMessagingClient(
+    private val client: HttpClient
+) : RealtimeMessagingClient {
+
+    private var session: WebSocketSession? = null
+
+    override fun getGameStateStream(): Flow<GameState> {
+        // this 10 lines get websocket msg and convert into real-time msg
+        return flow {
+            session = client.webSocketSession {
+                url("ws://192.168.1.1/play")
+            }
+            // we have a Flow of type GameState, which will trigger every time whenever a new msg
+            // is coming, it will automatically map that into an actual GameState object
+            val gameStates = session!!
+                .incoming
+                .consumeAsFlow() // in our case just text
+                .filterIsInstance<Frame.Text>()
+                .mapNotNull { Json.decodeFromString<GameState>(it.readText()) }
+
+            emitAll(gameStates) // this gives data to the flow
+        }
+    }
+
+    override suspend fun sendAction(action: MakeTurn) {
+        session?.outgoing?.send(
+            Frame.Text("make_turn#${Json.encodeToString(action)}")
+        )
+    }
+
+    override suspend fun close() {
+        session?.close()
+        session = null
+    }
+}
